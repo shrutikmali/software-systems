1d1fb1d6300674aff69459ea9347353c669e4265
Shrutik <shrutik.mali15@gmail.com>
2023-09-01 10:04:23 +0530

    24

---
+++ 24.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main() {
+	int cpid = fork();
+	if(!cpid) {
+		while(1);
+	}
+	else {
+		;
+	}
+	return 0;
+}

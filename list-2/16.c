922b5cf73f55ce748aeef0445178180f17e272ba
Shrutik <shrutik.mali15@gmail.com>
2023-09-06 18:22:33 +0530

    16

---
+++ list-2/16.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main() {
+	int fd1[2];
+	int fd2[2];
+	pipe(fd1);
+	pipe(fd2);
+
+	int pid = fork();
+	if(!pid) {
+		close(fd2[0]);
+		close(fd1[1]);
+		while(1) {
+			char readBuffer[20];
+			read(fd1[0], &readBuffer, sizeof(readBuffer));
+			printf("Message received from parent: %s\n", readBuffer);
+			printf("Enter message to send to parent: ");
+			char writeBuffer[20];
+			//scanf("%[^\n]", writeBuffer);
+			fgets(writeBuffer, sizeof(writeBuffer), stdin);
+			write(fd2[1], &writeBuffer, sizeof(writeBuffer));
+		}
+	}
+	else {
+		close(fd1[0]);
+		close(fd2[1]);
+		while(1) {
+			char writeBuffer[20];
+			printf("Enter message to send to child: ");
+			//scanf("%[^\n]", writeBuffer);
+			fgets(writeBuffer, sizeof(writeBuffer), stdin);
+			write(fd1[1], &writeBuffer, sizeof(writeBuffer));
+			char readBuffer[20];
+			read(fd2[0], &readBuffer, sizeof(readBuffer));
+			printf("Message received from child: %s\n", readBuffer);
+		}
+	}
+	return 0;
+}

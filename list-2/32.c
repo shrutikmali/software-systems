1358d162345d5b5dd29a692102367d7d797b71f4
Shrutik <shrutik.mali15@gmail.com>
2023-10-10 21:09:22 +0530

    32

---
+++ list-2/32.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <string.h>
+
+int main() {
+	char *shm = shmat(1, (void*)0, 0);
+	int initial = 0;
+	memcpy(shm, &initial, sizeof(initial));
+
+	int semid = 2;
+	/*
+	int set_res = semctl(semid, 0, SETVAL, 1);
+	if(set_res == -1) {
+		perror("Error setting initial value: ");
+		return -1;
+	}
+	*/
+	struct sembuf wait = {0, -1, 0};
+	printf("Entering critical section\n");
+	if(semop(semid, &wait, 1) == -1) {
+		perror("Error waiting for semaphore\n");
+		return -1;
+	}
+	printf("Entered critical section");
+	int ticket;
+	memcpy(&ticket, &shm, sizeof(ticket));
+	printf("Current ticket value: %d\n", ticket);
+	ticket++;
+	printf("New ticket value: %d\n", ticket);
+	memcpy(shm, &ticket, sizeof(ticket));
+	printf("Press any key to release critical section\n");
+	getchar();
+	struct sembuf signal = {0, 1, 0};
+	if(semop(semid, &signal, 1) == -1) {
+		perror("Error signalling semaphore: ");
+		return -1;
+	}
+	printf("Critical section release\n");
+	return 0;
+}

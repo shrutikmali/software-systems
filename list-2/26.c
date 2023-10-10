1622ba4d5e22f7c63635e4ef971f89d10f71f87b
Shrutik <shrutik.mali15@gmail.com>
2023-10-10 18:00:37 +0530

    26

---
+++ list-2/26.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <string.h>
+
+struct msgbuf {
+	long mtype;
+	char mtext[];
+};
+
+int main() {
+	key_t key = ftok(".", 0);
+    int queue = msgget(key, IPC_CREAT);
+    if(queue == -1) {
+    	printf("Error in making queue\n");
+        return -1;
+    }
+    char message[100] = "Message to queue";
+	int size = 100;
+	struct msgbuf msg;
+	msg.mtype = (long)1;
+	strcpy(msg.mtext, message);
+
+	int res = msgsnd(queue, &msg, size, 0);
+	if(res == -1) {
+		
+		perror("Error in sending message\n");
+		return -1;
+	}
+	printf("Message sent to queue\n");
+	return 0;
+}
